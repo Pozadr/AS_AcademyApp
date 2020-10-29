4ad37a2675e0795490c5c96a8aa11c553c4d9864
Pozadr <adpozor@gmail.com>
2020-10-29 16:49:49 +0100

    ProductApi

--- src/main/java/pl/pozadr/ksb2/ProductApi.java
+++ src/main/java/pl/pozadr/ksb2/ProductApi.java
@@ -1,4 +1,30 @@
 package pl.pozadr.ksb2;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products") // to avoid @PostMapping("/products") etc.
 public class ProductApi {
+
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public String getProducts() {
+        return "Hello World with GET";
+    }
+
+    @PostMapping
+    public String addProduct() {
+        return "Hello World with POST";
+    }
+
+    @PutMapping
+    public String modifyProduct() {
+        return "Hello World with PUT";
+    }
+
+    @DeleteMapping
+    public String deleteProduct() {
+        return "Hello World with DELETE";
+    }
+
 }
